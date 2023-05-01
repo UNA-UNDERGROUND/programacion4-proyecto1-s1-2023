7562a0a22e8d5b2994cf24f7425f7e479e664b8f
alexr <alexram999rc@gmail.com>
2023-04-30 23:44:13 -0600

    added list method to MedioPagoDAO

--- backend/src/main/java/net/rnvn/model/dao/MedioPagoDAO.java
+++ backend/src/main/java/net/rnvn/model/dao/MedioPagoDAO.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import net.rnvn.db.PolizasDAO;
 import net.rnvn.db.QueryGen;
@@ -95,6 +97,37 @@ public class MedioPagoDAO extends PolizasDAO{
         return result;
     }
 
+    public List<MedioPago> getListadoMedioPagoCliente(String idCliente) {
+        ArrayList<MedioPago> listadoMedioPago = null;
+        try (Connection conn = getConnection();
+                PreparedStatement stmt = conn.prepareStatement(SQL_LIST_MEDIO_PAGO_CLIENTE)) {
+            stmt.clearParameters();
+            stmt.setString(1, idCliente);
+            try (java.sql.ResultSet rs = stmt.executeQuery()) {
+                listadoMedioPago = new ArrayList<MedioPago>();
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    int numeroTarjeta = rs.getInt("numero_tarjeta");
+                    String nombreTitular = rs.getString("nombre_titular");
+                    LocalDateTime fechaVencimiento = rs.getTimestamp("fecha_vencimiento").toLocalDateTime();
+                    String cvv = rs.getString("cvv");
+                    MedioPago medioPago = new MedioPago(
+                            id,
+                            idCliente,
+                            numeroTarjeta,
+                            nombreTitular,
+                            fechaVencimiento,
+                            cvv);
+                    listadoMedioPago.add(medioPago);
+                }
+            }
+        } catch (Exception e) {
+            String className = this.getClass().getName();
+            System.err.println("[" + className + "] Error al obtener listado de medios de pago: " + e.getMessage());
+        }
+        return listadoMedioPago;
+    }
+
     // consultas SQL
 
     private static final String TABLE_NAME = "medio_pago";
@@ -117,4 +150,16 @@ public class MedioPagoDAO extends PolizasDAO{
         = QueryGen.genDeleteString(
             TABLE_NAME,
             new String[] {"id = ?"});
+
+    private static final String SQL_LIST_MEDIO_PAGO_CLIENTE //
+            = QueryGen.genSelectString(
+                    TABLE_NAME,
+                    new String[] {
+                            "id",
+                            "id_cliente",
+                            "numero_tarjeta",
+                            "nombre_titular",
+                            "fecha_vencimiento",
+                            "cvv" },
+                    new String[] { "id_cliente = ?" });
 }
