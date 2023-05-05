f311b112bb59bd68d11edc4ab27147ab41694068
alexr <alexram999rc@gmail.com>
2023-05-05 09:29:36 -0600

    actualizarDatos

--- frontend/src/main/java/net/rnvn/frontend/credenciales/GestorActualizarDatos.java
+++ frontend/src/main/java/net/rnvn/frontend/credenciales/GestorActualizarDatos.java
@@ -25,7 +25,7 @@ public class GestorActualizarDatos extends HttpServlet {
             throws ServletException, IOException {
 
 
-         Credenciales credenciales = validarCredenciales(request);
+        Credenciales credenciales = validarCredenciales(request);
 
          Cliente cliente = actualizarCliente(request);
         
